a88dd2a758082053202c5911e276db8d25735711
abufaruk5825-cybersecurity <245520414+abufaruk5825-cybersecurity@users.noreply.github.com>
2026-06-29 06:16:05 +0300

    add stopwatch functionality in JavaScript

---
+++ watch.js
@@ -0,0 +1,41 @@
+const clock=document.getElementById("clock");
+let timer=null;
+let starTime=0;
+let elapsedTime=0;
+let isRunning =false;
+
+function start(){
+if(!isRunning){
+  starTime=Date.now()-elapsedTime
+   timer= setInterval(update,10);
+   isRunning = true;
+}
+}
+function stop(){
+  if (isRunning) {
+    clearInterval (timer);
+    elapsedTime =Date.now()-starTime;
+    isRunning =false;
+  } 
+}
+function reset(){
+    clearInterval(timer)
+     starTime =0;
+    elapsedTime=0;
+    isRunning =false;
+    clock.textContent="00:00:00:00";
+}
+function update(){
+const currentTime = Date.now();
+elapsedTime = currentTime - starTime;
+let hours = Math.floor(elapsedTime/(1000*60*60));
+let minutes = Math.floor(elapsedTime/ (1000*60)%60);
+let seconds =Math.floor(elapsedTime/1000%60);
+let milliseconds= Math.floor(elapsedTime%1000/10);
+hours = String(hours).padStart(2,"0");
+minutes = String(minutes).padStart(2,"0");
+seconds = String(seconds).padStart(2,"0");
+milliseconds = String(milliseconds).padStart(2,"0");
+
+clock.textContent=`${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
